59f2f2a38457991e02735ed9697ef123749f8406
Natasha Valdez <valdez.natasha.m@gmail.com>
2024-02-14 16:52:37 -0300

    realize el primer ejercicio de array con meses

--- js/01-ejerc.js
+++ js/01-ejerc.js
@@ -1,1 +1,8 @@
-const meses = ["enero", "febrero" , "marzo"]
+const meses = ["enero", "febrero" , "marzo" , "abril" , "mayo" , "junio" , "julio" , "agosto" , "septiembre" , "octubre" , "noviembre" , "diciembre"]
+
+document.write(`<h2> Lista de Meses </h2>`)
+document.write(`<ul>`)
+for(let posicion=0; posicion < meses.length; posicion++){
+document.write(`<li>${meses[posicion]}</li>`)
+}
+document.write(`</ul>`)
